045f336930b62338ba4cf7da19b023062b64b258
SaraCaetano <134522922+SaraCaetano@users.noreply.github.com>
2023-11-10 12:29:15 -0300

    alerta

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 function mudarTexto() {
     document.getElementById("demo").innerHTML = "Texto alterado!";
 }
+function exibirAlerta() {
+    alert("Este é apenas um site teste! Não haverá atualizações futuramente.");
+}
